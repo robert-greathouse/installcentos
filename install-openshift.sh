37e613aa522504bf88c08e4f26813d7f8e1f3626
Robert Greathouse <robert.i.greathouse@gmail.com>
2018-04-03 11:15:34 -0700

    Fixed IP EV

--- install-openshift.sh
+++ install-openshift.sh
@@ -9,7 +9,7 @@ export VERSION=${VERSION:="v3.7.1"}
 
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/robert-greathouse/installcentos/master"}
 
-export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
+export IP=${IP}
 export API_PORT=${API_PORT:="8443"}
 
 echo "******"
